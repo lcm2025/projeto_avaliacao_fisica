1e99cd7716d0de23d5332ab3fa61e5af64f866a3
lcm2025 <lcm_cefet@hotmail.com>
2025-04-22 20:39:51 -0300

    Teste

--- avaliacao_fisica/avaliacao_fisica/settings.py
+++ avaliacao_fisica/avaliacao_fisica/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+
+#DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = [
     'https://projeto-avaliacao-fisica.onrender.com,'  # Substitua pelo URL do seu serviço no Render
